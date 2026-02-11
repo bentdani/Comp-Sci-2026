995719ef60910b729c051f88896454d7838b2633
bentdani <danielbento27@icloud.com>
2026-02-11 13:48:11 -0500

    bum

--- Scripts/track.gd
+++ Scripts/track.gd
@@ -1,1 +1,8 @@
 extends Node3D
+var intital_pos = Vector3(0, 20.552, 710.458)
+var intital_ros = Vector3(0, -100, 0)
+
+func _on_delete_body_entered(body: Node3D) -> void:
+	print("die kelean")
+	$Player_Car.position = intital_pos
+	$Player_Car.rotation = intital_ros
