97982a81707b6956167ba4184a68f6a5718dcdf4
Liam McDermott <mcdeliam34@gmail.com>
2026-02-13 14:08:39 -0500

    faaaaaaaaaaaaa

--- Scripts/track.gd
+++ Scripts/track.gd
@@ -1,8 +1,10 @@
 extends Node3D
-var intital_pos = Vector3(0, 20.552, 710.458)
-var intital_ros = Vector3(0, -100, 0)
+var intital_pos = Vector3(874.514, 5.702, -356.598)
+var intital_ros = Vector3(0, -119.2, 0)
 
 func _on_delete_body_entered(body: Node3D) -> void:
 	print("die kealen")
 	$Player_Car.position = intital_pos
 	$Player_Car.rotation = intital_ros
+	$Player_Car.linear_velocity = Vector3(0,0,0)
+	$Player_Car.angular_velocity = Vector3(0,0,0)
