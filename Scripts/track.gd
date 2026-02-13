3977a436f56d1f021defc2b2dfbf932739ed1ae4
Liam McDermott <mcdeliam34@gmail.com>
2026-02-13 13:51:39 -0500

    kealen

--- Scripts/track.gd
+++ Scripts/track.gd
@@ -3,6 +3,6 @@ var intital_pos = Vector3(0, 20.552, 710.458)
 var intital_ros = Vector3(0, -100, 0)
 
 func _on_delete_body_entered(body: Node3D) -> void:
-	print("die kelean")
+	print("die kealen")
 	$Player_Car.position = intital_pos
 	$Player_Car.rotation = intital_ros
