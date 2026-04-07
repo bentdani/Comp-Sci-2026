af2cda4502bc6650cf67e681332db524062539ad
bentdani <danielbento27@icloud.com>
2026-04-07 14:48:22 -0400

    dih

--- Scripts/track.gd
+++ Scripts/track.gd
@@ -2,7 +2,6 @@ extends Node3D
 var intital_pos = Vector3(874.514, 5.702, -356.598)
 var intital_ros = Vector3(0, -119.2, 0)
 var title = load("res://Scenes/title.tscn")
-var laps = 0
 var can_score = true
 
 func _on_delete_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
@@ -18,7 +17,6 @@ func _on_delete_body_shape_entered(body_rid, body, body_shape_index, local_shape
 		# 2. Kill all momentum
 		body.linear_velocity = Vector3.ZERO
 		body.angular_velocity = Vector3.ZERO
-		laps = 0
 		can_score = true
 
 func _process(_delta):
@@ -27,20 +25,19 @@ func _process(_delta):
 	# Add this here to update every frame:
 	var kmh = $Player_Car.linear_velocity.length() * 2.0
 	$timer_ui/CanvasLayer/SpeedLabel.text = str(round(kmh)) + " km/h"
-	Global.current_lap = laps
-	if laps == 4:
+	if Global.current_lap == 4:
 		get_parent().add_child(title.instantiate())
 		queue_free()
 		
 
 func _on_p_1_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.name == "Player_Car" and can_score == true:
-		laps += 1
+		Global.current_lap += 1
 		can_score = false
 	
 func _on_p_1_body_shape_exited(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.name == "Player_Car":
-		print(laps)
+		print(Global.current_lap)
 		
 func _on_checkpoint_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.name == "Player_Car":
